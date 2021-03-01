65415fd826ee27b27a1610f056df9c94bf6f39de
MatthewADIAN <68260159+MatthewADIAN@users.noreply.github.com>
2021-03-01 15:29:21 +0700

    Initial commit

---
+++ src/main/java/com/websuhu/Frameworksuhu/FrameworksuhuApplication.java
@@ -0,0 +1,13 @@
+package com.websuhu.Frameworksuhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksuhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworksuhuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websuhu/Frameworksuhu/FrameworksuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websuhu.Frameworksuhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworksuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
